eb4cd768a9645ce505b8b29918b3a500a02a4478
zong <69517696+caizongxun@users.noreply.github.com>
2026-02-24 22:55:04 +0800

    Add ModelManagementTab to tabs

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -1,13 +1,17 @@
 from .data_fetcher_tab import DataFetcherTab
 from .feature_engineering_tab import FeatureEngineeringTab
 from .model_training_tab import ModelTrainingTab
+from .model_training_v2_tab import ModelTrainingV2Tab
 from .backtesting_tab import BacktestingTab
 from .auto_trading_tab import AutoTradingTab
+from .model_management_tab import ModelManagementTab
 
 __all__ = [
     'DataFetcherTab',
     'FeatureEngineeringTab',
     'ModelTrainingTab',
+    'ModelTrainingV2Tab',
     'BacktestingTab',
-    'AutoTradingTab'
+    'AutoTradingTab',
+    'ModelManagementTab'
 ]
